5c787192b0f950c5ca76863cb7a420182eff1454
am1cable <tegan.mcfa@gmail.com>
2021-02-21 16:59:02 -0500

    new style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from "react";
 import Pages from "./pages/pages";
 import { HashRouter as Router } from "react-router-dom";
 import "./App.scss";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#FF6A00',
+      main: '#FF6A00',
+      dark: '#FF6A00',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 export const App = () => (
   <div className="daily-planner">
-    <Router>
+    <Router><ThemeProvider theme={theme}>
     <Pages />
+    </ThemeProvider>
   </Router>
   </div>
 );
--- src/components/navButtons/navButtons.js
+++ src/components/navButtons/navButtons.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Button, ButtonGroup } from "@material-ui/core";
 import { Link } from 'react-router-dom';
 import StructureContext from "../context/structureContext";
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import "./navButtons.scss";
 
 const NavButtonLinks = ({ text, link, ...props }) => <NavButton {...props} component={Link} to={link}>{text}</NavButton>
 
@@ -11,12 +13,12 @@ const emptyDirection = { disabled: true, link: "/" }
 
 export const NavButtons = ({ back = emptyDirection, forward = emptyDirection }) => {
     return <StructureContext.Consumer>
-        {({ onNavigateForwards }) => <ButtonGroup color={"primary"} size="large" variant="contained">
+        {({ onNavigateForwards }) => <ButtonGroup class="navButtons" color={"primary"} size="large" variant="contained">
             {/* {back != emptyDirection && <NavButton {...back}>
                 {back.name ? `back to ${back.name}` : 'back'}
             </NavButton>} */}
             {forward != emptyDirection && <NavButton {...forward} onClick={onNavigateForwards}>
-                {forward.name ? `forward to ${forward.name}` : 'forward'}
+                <ArrowForwardIcon/>
             </NavButton>}
         </ButtonGroup>
         }
